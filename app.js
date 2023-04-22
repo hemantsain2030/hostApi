1d92e1d002f6254793df473bac750b7b1e9b481d
Hemant <hemantsain2030@gmail.com>
2023-04-22 09:48:27 +0530

    host api live

---
+++ app.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./db/connect")
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+const products_routes = require("./routes/products");
+
+app.get("/", (req, res) => {
+    res.send("Hi, I am live");
+});
+
+// middleware or set router
+app.use("/api/products", products_routes);
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGODB_URL);
+        app.listen(PORT, () => {
+            console.log(`${PORT} Yes I am connected`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
